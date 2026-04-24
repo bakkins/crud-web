6315336e0af4c6f483ecc5693790b163fba4f2bd
bakkins <llaurisb07@gmail.com>
2026-04-24 14:08:59 +0300

    added authroutes

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\PostController;
+use App\Http\Controllers\Api\AuthController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -11,3 +12,11 @@ Route::get('/user', function (Request $request) {
 Route::apiResource('posts', PostController::class);
 
 Route::post('/posts', [PostController::class, 'store']);
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login',    [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout',       [AuthController::class, 'logout']);
+    Route::apiResource('posts',  PostController::class);
+});
