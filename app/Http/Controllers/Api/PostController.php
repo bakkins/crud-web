ee9ccf5df4cbc721b57bd3e4173f1771249d93a1
bakkins <llaurisb07@gmail.com>
2026-04-24 13:38:31 +0300

    PostController api

---
+++ app/Http/Controllers/Api/PostController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function index()
+    {
+        return response()->json(Post::latest()->get());
+    }
+
+    public function store(Request $request)
+    {
+        $data = $request->validate([
+            'title'   => 'required|string|max:255',
+            'content' => 'required|string',
+        ]);
+
+        $post = Post::create($data);
+        return response()->json($post, 201);
+    }
+
+    public function show(Post $post)
+    {
+        return response()->json($post);
+    }
+
+    public function update(Request $request, Post $post)
+    {
+        $data = $request->validate([
+            'title'   => 'required|string|max:255',
+            'content' => 'required|string',
+        ]);
+
+        $post->update($data);
+        return response()->json($post);
+    }
+
+    public function destroy(Post $post)
+    {
+        $post->delete();
+        return response()->json(['message' => 'Dzēsts']);
+    }
+}
